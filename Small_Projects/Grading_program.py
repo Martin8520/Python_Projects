c605a08f693076133fb529d10bef8440f8d37b52
Martin8520 <martin.b8520@gmail.com>
2024-02-02 14:15:20 +0200

    Adding Grading_program.py

---
+++ Small_Projects/Grading_program.py
@@ -0,0 +1,26 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99,
+  "Draco": 74,
+  "Neville": 62,
+}
+# 🚨 Don't change the code above 👆
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to student_grades.👇
+for student, score in student_scores.items():
+    if 91 <= score <= 100:
+        grade = "Outstanding"
+    elif 81 <= score <= 90:
+        grade = "Exceeds Expectations"
+    elif 71 <= score <= 80:
+        grade = "Acceptable"
+    else:
+        grade = "Fail"
+
+    student_grades[student] = grade
+# 🚨 Don't change the code below 👇
+print(student_grades)
+
