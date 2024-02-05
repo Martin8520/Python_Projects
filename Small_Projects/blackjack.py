2fb2bcdef538af20d996ed0cdd5a1bafbe670395
Martin8520 <martin.b8520@gmail.com>
2024-02-05 09:19:34 +0200

    Removed extra list

--- Small_Projects/blackjack.py
+++ Small_Projects/blackjack.py
@@ -2,8 +2,6 @@ import random
 
 from Small_Projects.blackjack_art import logo
 
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
 
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
