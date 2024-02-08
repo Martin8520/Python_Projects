334b73a95ad6b930da52ad8e4dfaa11e5fdcb124
Martin8520 <martin.b8520@gmail.com>
2024-02-08 12:09:02 +0200

    quiz_game WIP 4 added final score

--- Small_Projects/quiz_game/main.py
+++ Small_Projects/quiz_game/main.py
@@ -14,3 +14,7 @@ quiz.next_question()
 
 while quiz.still_has_questions():
     quiz.next_question()
+
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{len(question_bank)}")
