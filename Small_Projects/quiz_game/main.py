2199943e0858ed131bebd80f317b0c00a0c276df
Martin8520 <martin.b8520@gmail.com>
2024-02-08 11:44:21 +0200

    quiz_game WIP

--- Small_Projects/quiz_game/main.py
+++ Small_Projects/quiz_game/main.py
@@ -1,4 +1,4 @@
-from Small_Projects.quiz_game.quiz_brain import QuizBrain
+from quiz_brain import QuizBrain
 from question_model import Question
 from data import question_data
 
@@ -9,4 +9,5 @@ for question in question_data:
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-QuizBrain(question_bank)
+quiz = QuizBrain(question_bank)
+quiz.next_question()
