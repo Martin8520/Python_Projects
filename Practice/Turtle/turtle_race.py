febc51ea734f69b0a41a6261b3d1ede80bac5adb
Martin8520 <martin.b8520@gmail.com>
2024-02-10 10:53:04 +0200

    Added turtle_race.py optimized Turtle generation and betting.

--- Practice/Turtle/turtle_race.py
+++ Practice/Turtle/turtle_race.py
@@ -1,44 +1,37 @@
-from turtle import *
-from random import *
 
+from turtle import *
+from random import randint
+
+is_race_on = False
 screen = Screen()
+screen.setup(500, 400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 print(user_bet)
+y_pos = [-70, -40, -10, 20, 50, 80]
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
 
-tim = Turtle()
-tim.shape("turtle")
-tim.color("blue")
-tim.penup()
-tim.goto(-200, 200)
+for turtle_i in range(0, 6):
+    new_turtle = Turtle()
+    new_turtle.shape("turtle")
+    new_turtle.color(colors[turtle_i])
+    new_turtle.penup()
+    new_turtle.goto(-230, y=y_pos[turtle_i])
+    all_turtles.append(new_turtle)
+if user_bet:
+    is_race_on = Turtle
 
-tom = Turtle()
-tom.shape("turtle")
-tom.color("green")
-tom.penup()
-tom.goto(-200, 180)
+while is_race_on:
 
-tam = Turtle()
-tam.shape("turtle")
-tam.color("purple")
-tam.penup()
-tam.goto(-200, 160)
-
-jim = Turtle()
-jim.shape("turtle")
-jim.color("red")
-jim.penup()
-jim.goto(-200, 140)
-
-jam = Turtle()
-jam.shape("turtle")
-jam.color("teal")
-jam.penup()
-jam.goto(-200, 120)
-
-pam = Turtle()
-pam.shape("turtle")
-pam.color("pink")
-pam.penup()
-pam.goto(-200, 100)
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner")
+            else:
+                print(f"You've lost! The {winning_color} turtle is the winner")
+        speed = randint(0, 10)
+        turtle.forward(speed)
 
 screen.exitonclick()
