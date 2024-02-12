aee8cae3f5a582d5eabdcea303b94f07a760a70a
Martin8520 <martin.b8520@gmail.com>
2024-02-12 12:21:43 +0200

    Added wall collision and a Game Over message

--- Small_Projects/snake_game/scoreboard.py
+++ Small_Projects/snake_game/scoreboard.py
@@ -1,4 +1,5 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
 FONT = ("Courier", 18, "normal")
 
@@ -16,6 +17,10 @@ class Scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(arg=f"Score: {self.score} ", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg=f"GAME OVER", align=ALIGNMENT, font=FONT)
 
     def count_score(self):
         self.score += 1
