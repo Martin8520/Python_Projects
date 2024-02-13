b36a445d14cc4fb2df6c7427e0162be8b55be3eb
Martin8520 <martin.b8520@gmail.com>
2024-02-13 11:57:07 +0200

    Added scoreboard.py that adds a scoring system to pong.

--- Small_Projects/pong/main.py
+++ Small_Projects/pong/main.py
@@ -1,8 +1,9 @@
 import time
 from turtle import Screen, Turtle
 
-from Small_Projects.pong.paddle import Paddle
+from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(800, 600)
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -32,7 +34,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380:
         ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
 
 screen.exitonclick()
