c4d7e28ab850e6d5b56f133ad9e0778bc2d051ed
Martin8520 <martin.b8520@gmail.com>
2024-02-17 10:42:24 +0200

    Added practice code for list comprehensions.

--- Practice/list_comprehensions.py
+++ Practice/list_comprehensions.py
@@ -2,6 +2,9 @@
 # new_list = [n + 1 for n in numbers]
 # print(new_list)
 
-name = "Martin"
-new_list = [letter for letter in name]
+# name = "Martin"
+# new_list = [letter for letter in name]
+# print(new_list)
+
+new_list = [n * 2 for n in range(1, 5)]
 print(new_list)
