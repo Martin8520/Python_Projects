bc108d229d65c303e982ae99905e86926cfd92dd
Martin8520 <martin.b8520@gmail.com>
2024-02-18 09:55:26 +0200

    Added practice code for list comprehensions.

--- Practice/list_comprehensions.py
+++ Practice/list_comprehensions.py
@@ -6,5 +6,10 @@
 # new_list = [letter for letter in name]
 # print(new_list)
 
-new_list = [n * 2 for n in range(1, 5)]
-print(new_list)
+# new_list = [n * 2 for n in range(1, 5)]
+# print(new_list)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+
+capitalized_names = [name.upper() for name in names if len(name) > 5]
+print(capitalized_names)
