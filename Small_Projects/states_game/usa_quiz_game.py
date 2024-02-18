8544b6de8f36278ad5df6b7c0b2ebd4574470630
Martin8520 <martin.b8520@gmail.com>
2024-02-18 12:41:30 +0200

    Made the usa_quiz_game.py into an executable.

--- Small_Projects/states_game/usa_quiz_game.py
+++ Small_Projects/states_game/usa_quiz_game.py
@@ -3,24 +3,20 @@ import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
-image = "blank_states_img.gif"
+image = "/Users/marti/PycharmProjects/self_prep/Small_Projects/states_game/blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
-data = pandas.read_csv("50_states.csv")
+data = pandas.read_csv("/Users/marti/PycharmProjects/self_prep/Small_Projects/states_game/50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What is the state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
-        print(*missing_states)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+        new_data.to_csv("/Users/marti/PycharmProjects/self_prep/Small_Projects/states_game/states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
