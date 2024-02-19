6232c7694089de83615b379d6cc54aece429eab7
Martin8520 <martin.b8520@gmail.com>
2024-02-19 13:23:48 +0200

    Added Tkinter_practice folder.

---
+++ Small_Projects/Tkinter_practice/main.py
@@ -0,0 +1,23 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+my_label = tkinter.Label(text="TEST LABEL", font=("Arial", 24, "bold"))
+my_label.pack()
+
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
+
