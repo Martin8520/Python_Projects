6b00dc027e1937792eee54d80bac7c7fba714528
Martin8520 <martin.b8520@gmail.com>
2024-02-20 12:23:17 +0200

    Added a clickable button to main.py.

--- Practice/Tkinter_practice/main.py
+++ Practice/Tkinter_practice/main.py
@@ -1,23 +1,26 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
-my_label = tkinter.Label(text="TEST LABEL", font=("Arial", 24, "bold"))
+my_label = Label(text="TEST LABEL", font=("Arial", 24, "bold"))
 my_label.pack()
 
+my_label["text"] = "New text"
+
+my_label.config(text="New Text")
 
 
+def button_clicked():
+    my_label.config(text="Button Got Clicked")
 
 
-
-
-
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
 
 
 
 
 
 window.mainloop()
-
