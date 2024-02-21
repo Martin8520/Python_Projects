8da76e9c600b9bfab4a79b0c263142b8c82923f0
Martin8520 <martin.b8520@gmail.com>
2024-02-21 13:57:18 +0200

    Added Pomodoro timer functionality (WIP).

--- Small_Projects/Pomodoro/main.py
+++ Small_Projects/Pomodoro/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,11 +10,22 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5 * 60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, countdown, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -24,7 +36,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato)
-canvas.create_text(100, 130, text="00:00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(row=1, column=1)
 
 timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), highlightthickness=0, bg=YELLOW)
@@ -33,7 +45,7 @@ timer.grid(row=0, column=1)
 check = Label(text="✔", fg=GREEN, font="bold", bg=YELLOW)
 check.grid(row=3, column=1)
 
-start = Button(text="Start", bg="white", font=(FONT_NAME, 10, "bold"), highlightthickness=0)
+start = Button(text="Start", bg="white", font=(FONT_NAME, 10, "bold"), highlightthickness=0, command=start_timer)
 start.grid(row=2, column=0)
 
 reset = Button(text="Reset", bg="white", font=(FONT_NAME, 10, "bold"), highlightthickness=0)
