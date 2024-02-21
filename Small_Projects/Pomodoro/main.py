2798351ec924c0a71ecfea6356c52752cc4f0b29
Martin8520 <martin.b8520@gmail.com>
2024-02-21 16:11:58 +0200

    Timer functionality for Pomodoro - COMPLETE Reset function - PENDING Text Label - PENDING
    Changed design of app.

--- Small_Projects/Pomodoro/main.py
+++ Small_Projects/Pomodoro/main.py
@@ -10,13 +10,21 @@ BUTTON_COLOR = "#F72798"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+LONG_BREAK_MIN = 15
 REPS = 0
+timer_count = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset():
+    window.after_cancel(timer_count)
+    timer.config(text="Timer", fg=GREEN, font=(FONT_NAME, 30, "bold"), highlightthickness=0, bg=YELLOW)
+    check.config(text="", fg=GREEN, font="bold", bg="black")
+    canvas.itemconfig(timer_text, text="00:00")
+    global REPS
+    REPS = 0
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global REPS
     REPS += 1
@@ -25,10 +33,13 @@ def start_timer():
     long_break_sec = LONG_BREAK_MIN * 60
     if REPS % 8 == 0:
         countdown(long_break_sec)
+        timer.config(text="Break", fg=RED)
     elif REPS % 2 == 0:
         countdown(short_break_sec)
+        timer.config(text="Break", fg=PINK)
     else:
         countdown(work_sec)
+        timer.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -40,9 +51,14 @@ def countdown(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, countdown, count - 1)
+        global timer_count
+        timer_count = window.after(1000, countdown, count - 1)
     else:
         start_timer()
+        marks = ""
+        for _ in range(math.floor(REPS / 2)):
+            marks += "✔"
+        check.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -60,12 +76,12 @@ canvas.grid(row=1, column=1)
 timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 30, "bold"), highlightthickness=0, bg=YELLOW)
 timer.grid(row=0, column=1)
 
-check = Label(text="✔", fg=GREEN, font="bold", bg=YELLOW)
+check = Label(fg=GREEN, font="bold", bg="black")
 check.grid(row=3, column=1)
 
 start = Button(text="START", bg=BUTTON_COLOR, font=(FONT_NAME, 15, "bold"), highlightthickness=0, command=start_timer)
 start.grid(row=2, column=0)
 
-reset = Button(text="RESET", bg=BUTTON_COLOR, font=(FONT_NAME, 15, "bold"), highlightthickness=0)
+reset = Button(text="RESET", bg=BUTTON_COLOR, font=(FONT_NAME, 15, "bold"), highlightthickness=0, command=reset)
 reset.grid(row=2, column=2)
 window.mainloop()
