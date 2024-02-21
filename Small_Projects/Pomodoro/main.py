38ba8e62a7c8f9001b14cc1a18f2586fde545f24
Martin8520 <martin.b8520@gmail.com>
2024-02-21 14:30:17 +0200

    Timer functionality for Pomodoro - COMPLETE Reset function - PENDING Text Label - PENDING
    Changed design of app.

--- Small_Projects/Pomodoro/main.py
+++ Small_Projects/Pomodoro/main.py
@@ -4,8 +4,9 @@ import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+GREEN = "#416D19"
+YELLOW = "#FFF67E"
+BUTTON_COLOR = "#F72798"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -48,23 +49,23 @@ def countdown(count):
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg="black")
 
-canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+canvas = Canvas(width=200, height=224, bg="black", highlightthickness=0)
 tomato = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
 canvas.grid(row=1, column=1)
 
-timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 40, "bold"), highlightthickness=0, bg=YELLOW)
+timer = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 30, "bold"), highlightthickness=0, bg=YELLOW)
 timer.grid(row=0, column=1)
 
 check = Label(text="✔", fg=GREEN, font="bold", bg=YELLOW)
 check.grid(row=3, column=1)
 
-start = Button(text="Start", bg="white", font=(FONT_NAME, 10, "bold"), highlightthickness=0, command=start_timer)
+start = Button(text="START", bg=BUTTON_COLOR, font=(FONT_NAME, 15, "bold"), highlightthickness=0, command=start_timer)
 start.grid(row=2, column=0)
 
-reset = Button(text="Reset", bg="white", font=(FONT_NAME, 10, "bold"), highlightthickness=0)
+reset = Button(text="RESET", bg=BUTTON_COLOR, font=(FONT_NAME, 15, "bold"), highlightthickness=0)
 reset.grid(row=2, column=2)
 window.mainloop()
