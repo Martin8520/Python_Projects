ce0307713e7117a13d3271e366f26ad93d9abd45
Martin8520 <martin.b8520@gmail.com>
2024-02-22 11:42:05 +0200

    added a save to file functionality.

--- Small_Projects/password_manager/main.py
+++ Small_Projects/password_manager/main.py
@@ -1,7 +1,18 @@
 from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+    website = website_entry.get()
+    email_user = email_username_entry.get()
+    password = password_entry.get()
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} || {email_user} || {password}\n")
+    website_entry.delete(0, END)
+    email_username_entry.delete(0, END)
+    password_entry.delete(0, END)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -33,17 +44,7 @@ password_entry.grid(row=3, column=1)
 password_button = Button(text="Generate Password")
 password_button.grid(row=3, column=2)
 
-add_button = Button(text="Add", width=42)
+add_button = Button(text="Add", width=42, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
-
-
-
-
-
-
-
-
-
-
 
 window.mainloop()
