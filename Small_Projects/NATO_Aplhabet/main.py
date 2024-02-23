1633aae411e8c1cb80b5a062e340409dd70be71d
Martin8520 <martin.b8520@gmail.com>
2024-02-23 12:22:37 +0200

    Added KeyError exception in NATO alphabet code.

--- Small_Projects/NATO_Aplhabet/main.py
+++ Small_Projects/NATO_Aplhabet/main.py
@@ -4,7 +4,17 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
-word = input("Enter word: ").upper()
-output = [phonetic_dict[letter] for letter in word]
-for item in output:
-    print(item)
+
+def gen_phonetic():
+    word = input("Enter word: ").upper()
+    try:
+        output = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Please enter a valid word.")
+        gen_phonetic()
+    else:
+        for letter in output:
+            print(letter)
+
+
+gen_phonetic()
