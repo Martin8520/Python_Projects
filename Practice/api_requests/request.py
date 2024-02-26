31ec16949ddc548eab45aad0229402232c3bf42f
Martin8520 <martin.b8520@gmail.com>
2024-02-26 11:11:29 +0200

    Added exceptions for error codes with raise_for_status() function.

--- Practice/api_requests/request.py
+++ Practice/api_requests/request.py
@@ -1,7 +1,4 @@
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-if response.status_code == 404:
-    raise Exception("That resource does not exist.")
-elif response.status_code == 401:
-    raise Exception("You are not authorised to access that data.")
+response.raise_for_status()
