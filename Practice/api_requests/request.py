709f8aece7b090aea9ec4c67c78cb11e48b2525b
Martin8520 <martin.b8520@gmail.com>
2024-02-26 11:17:04 +0200

    Requested data for latlong of the ISS.

--- Practice/api_requests/request.py
+++ Practice/api_requests/request.py
@@ -2,3 +2,8 @@ import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
+
+data = response.json()["iss_position"]["longitude"]
+data2 = response.json()["iss_position"]["latitude"]
+iss_position = (data, data2)
+print(iss_position)
