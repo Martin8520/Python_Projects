ee4a53dd8ac7610a2e3e1e4abd1db9cecbce9113
Martin8520 <martin.b8520@gmail.com>
2024-03-01 11:16:11 +0200

    Added age to seconds converter to age_to_month.py.

--- Practice/age_to_month.py
+++ Practice/age_to_month.py
@@ -2,3 +2,6 @@ user_age = int(input("Enter your age:"))
 
 months = user_age * 12
 print(f"Your age, {user_age}, is equal to {months} months.")
+
+seconds = user_age * 3156300
+print(f"Your age, {user_age}, is equal to {seconds} seconds")
