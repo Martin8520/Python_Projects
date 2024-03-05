81e6381b13845a6f2d3e7eaf8b7006456943cce0
Martin8520 <martin.b8520@gmail.com>
2024-03-05 12:20:52 +0200

    Mutable default parameters (and why they're a bad idea)

--- Practice/mutability_practice.py
+++ Practice/mutability_practice.py
@@ -1,6 +1,25 @@
-a = "hello"
-print(a)
-b = 8555
-a += " world"
-print(a)
-print(b)
+# a = "hello"
+# print(a)
+# b = 8555
+# a += " world"
+# print(a)
+# print(b)
+from typing import List, Optional
+
+
+class Student:
+    # def __init__(self, name: str, grades: List[int] = []):  # This is bad
+    def __init__(self, name: str, grades: Optional[List[int]] = None):  # This is good
+
+        self.name = name
+        self.grades = grades or []
+
+    def take_exam(self, result):
+        self.grades.append(result)
+
+
+ralf = Student("Ralf")
+bob = Student("Bob")
+bob.take_exam(90)
+print(bob.grades)
+print(ralf.grades)
