26a75a290e3f60fb85440d664ca29de6276ef72b
Martin8520 <martin.b8520@gmail.com>
2024-03-12 13:10:26 +0200

    Data Structures practice with Jinja - lists

--- Practice/flask/app.py
+++ Practice/flask/app.py
@@ -4,27 +4,13 @@ from jinja2 import Template
 app = Flask(__name__)
 
 
-@app.route("/expressions/")
-def hello_world():
-    color = "brown"
-    animal_one = "fox"
-    animal_two = "dog"
+@app.route("/data_structures/")
+def render_data_structures():
 
-    orange_amount = 10
-    apple_amount = 20
-    donate_amount = 15
+    movies = [
+        "Leon the Professional",
+        "The Usual Suspects",
+        "A Beautiful Mind"
+    ]
 
-    first_name = "Captain"
-    last_name = "Marvel"
-
-    kwargs = {
-        "color": color,
-        "animal_one": animal_one,
-        "animal_two": animal_two,
-        "orange_amount": 10,
-        "apple_amount": 20,
-        "donate_amount": 15,
-        "first_name": "Captain",
-        "last_name": "Marvel",
-        }
-    return render_template("expressions.html", **kwargs)
+    return render_template("data_structures.html", movies=movies)
