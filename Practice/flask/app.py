af7549abe22e8e4d255cbc698eae680866501251
Martin8520 <martin.b8520@gmail.com>
2024-03-12 12:40:32 +0200

    Jinja practice

--- Practice/flask/app.py
+++ Practice/flask/app.py
@@ -4,10 +4,27 @@ from jinja2 import Template
 app = Flask(__name__)
 
 
-
-@app.route("/")
+@app.route("/expressions/")
 def hello_world():
-    return render_template("jinja_intro.html", name="Bob Smith", template_name="Jinja2")
+    color = "brown"
+    animal_one = "fox"
+    animal_two = "dog"
 
+    orange_amount = 10
+    apple_amount = 20
+    donate_amount = 15
 
+    first_name = "Captain"
+    last_name = "Marvel"
 
+    kwargs = {
+        "color": color,
+        "animal_one": animal_one,
+        "animal_two": animal_two,
+        "orange_amount": 10,
+        "apple_amount": 20,
+        "donate_amount": 15,
+        "first_name": "Captain",
+        "last_name": "Marvel",
+        }
+    return render_template("expressions.html", **kwargs)
