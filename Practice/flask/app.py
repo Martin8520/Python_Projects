c73db8138df52ae53788cc7a758404e780b4984a
Martin8520 <martin.b8520@gmail.com>
2024-03-12 13:24:52 +0200

    Data Structures practice with Jinja - Dictionaries and Classes.

--- Practice/flask/app.py
+++ Practice/flask/app.py
@@ -3,6 +3,12 @@ from jinja2 import Template
 
 app = Flask(__name__)
 
+class GalileanMoons:
+    def __init__(self, first, second, third, fourth):
+        self.first = first
+        self.second = second
+        self.third = third
+        self.fourth = fourth
 
 @app.route("/data_structures/")
 def render_data_structures():
@@ -13,4 +19,17 @@ def render_data_structures():
         "A Beautiful Mind"
     ]
 
-    return render_template("data_structures.html", movies=movies)
+    car = {
+        "brand": "BMW",
+        "model": "M5",
+        "year": "2020",
+    }
+
+    moons = GalileanMoons("Io", "Europa", "Ganymede", "Callisto")
+
+    kwargs = {
+        "movies": movies,
+        "car": car,
+        "moons": moons,
+    }
+    return render_template("data_structures.html", movies=movies, car=car, moons=moons)
