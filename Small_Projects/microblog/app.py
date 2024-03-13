a885a4977df2fa040baf0c8cb0dc79c33640d0a4
Martin8520 <martin.b8520@gmail.com>
2024-03-13 12:21:20 +0200

    MongoDB connected

--- Small_Projects/microblog/app.py
+++ Small_Projects/microblog/app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template, request
 import datetime
-
+from pymongo import MongoClient
 app = Flask(__name__)
-
-entries = []
+client = MongoClient("mongodb+srv://martinb8520:bkS1P8lBgs7k5Oa8@microblogapplication.iw5kcwd.mongodb.net")
+app.db = client.microblog
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -11,14 +11,14 @@ def home():
     if request.method == "POST":
         entry_content = request.form.get("content")
         formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-        entries.append((entry_content, formatted_date))
+        app.db.entries.insert_one({"content": entry_content, "date": formatted_date})
 
     entries_with_date = [
         (
-            entry[0],
-            entry[1],
-            datetime.datetime.strptime(entry[1], "%Y-%m-%d").strftime("%b %d")
+            entry["content"],
+            entry["date"],
+            datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
         )
-        for entry in entries
+        for entry in app.db.entries.find({})
     ]
     return render_template("home.html", entries=entries_with_date)
