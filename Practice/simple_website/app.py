d3ab5ec5d3a864529f1aa7c71d99d2805af18299
Martin8520 <martin.b8520@gmail.com>
2024-03-14 13:36:30 +0200

    Website with some hardcoded entries (WIP)

--- Practice/simple_website/app.py
+++ Practice/simple_website/app.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+entries = []
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', entries=entries)
+
+
+@app.route('/add_entry', methods=['POST'])
+def add_entry():
+    title = request.form['title']
+    content = request.form['content']
+    entries.append({'title': title, 'content': content})
+    return redirect('/')
 
 
 if __name__ == '__main__':
