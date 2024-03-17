234b0c498c13d4daf968005aa4ed217c5f13e6e4
Martin8520 <martin.b8520@gmail.com>
2024-03-17 13:06:52 +0200

    Habit Tracker: added a date_range function in add habit section.

--- Small_Projects/habit_tracker/app.py
+++ Small_Projects/habit_tracker/app.py
@@ -4,15 +4,23 @@ import datetime
 app = Flask(__name__)
 habits = ["Test habit", "Test habit 2", "Rest"]
 
-
-def date_range(start: datetime.date):
-    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
-    return dates
+@app.context_processor
+def add_calc_date_range():
+    def date_range(start: datetime.date):
+        dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+        return dates
+    return {"date_range": date_range}
 
 
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home", date_range=date_range)
+    date_str = request.args.get("date")
+    if date_str:
+        selected_date = datetime.date.fromisoformat(date_str)
+    else:
+        selected_date = datetime.date.today()
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home",
+                           selected_date=selected_date)
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -20,4 +28,4 @@ def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
 
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit")
+    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
