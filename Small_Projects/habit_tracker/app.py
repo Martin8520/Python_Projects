821d91d7a8cc789ce20ebcc8b19923f05f7a4567
Martin8520 <martin.b8520@gmail.com>
2024-03-17 12:34:59 +0200

    Habit Tracker: added a date_range function in app.py.

--- Small_Projects/habit_tracker/app.py
+++ Small_Projects/habit_tracker/app.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template, request
+import datetime
 
 app = Flask(__name__)
 habits = ["Test habit", "Test habit 2", "Rest"]
 
 
+def date_range(start: datetime.date):
+    dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
+    return dates
+
+
 @app.route("/")
 def index():
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home")
+    return render_template("index.html", habits=habits, title="Habit Tracker - Home", date_range=date_range)
 
 
 @app.route("/add", methods=["GET", "POST"])
