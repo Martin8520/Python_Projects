02f889030c5d4526cf88174405252ded9a1f6c64
Martin8520 <martin.b8520@gmail.com>
2024-03-18 09:50:52 +0200

    Habit Tracker: Habits are now buttons that display a check mark when clicked. Styling pending.

--- Small_Projects/habit_tracker/app.py
+++ Small_Projects/habit_tracker/app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import datetime
+from collections import defaultdict
 
 app = Flask(__name__)
 habits = ["Test habit", "Test habit 2", "Rest"]
+completions = defaultdict(list)
+
 
 @app.context_processor
 def add_calc_date_range():
     def date_range(start: datetime.date):
         dates = [start + datetime.timedelta(days=diff) for diff in range(-3, 4)]
         return dates
+
     return {"date_range": date_range}
 
 
@@ -19,8 +23,11 @@ def index():
         selected_date = datetime.date.fromisoformat(date_str)
     else:
         selected_date = datetime.date.today()
-    return render_template("index.html", habits=habits, title="Habit Tracker - Home",
-                           selected_date=selected_date)
+    return render_template("index.html",
+                           habits=habits,
+                           title="Habit Tracker - Home",
+                           selected_date=selected_date,
+                           completions=completions[selected_date])
 
 
 @app.route("/add", methods=["GET", "POST"])
@@ -28,4 +35,16 @@ def add_habit():
     if request.method == "POST":
         habits.append(request.form.get("habit"))
 
-    return render_template("add_habit.html", title="Habit Tracker - Add Habit", selected_date=datetime.date.today())
+    return render_template("add_habit.html",
+                           title="Habit Tracker - Add Habit",
+                           selected_date=datetime.date.today())
+
+
+@app.route("/complete", methods=["post"])
+def complete():
+    date_string = request.form.get("date")
+    habit = request.form.get("habitName")
+    date = datetime.date.fromisoformat(date_string)
+    completions[date].append(habit)
+
+    return redirect(url_for("index", date=date_string))
