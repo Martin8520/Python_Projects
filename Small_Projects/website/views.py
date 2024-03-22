c8d89a787217106800f21bcc0e8abad6b66e2a02
Martin8520 <martin.b8520@gmail.com>
2024-03-22 12:07:06 +0200

    Route and HTML practice.

--- Small_Projects/website/views.py
+++ Small_Projects/website/views.py
@@ -1,8 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
 
 @views.route("/")
 def home():
-    return "home page"
+    return render_template("index.html", name="Joe")
+
+
+@views.route("/profile/<username>")
+def profile(username):
+    return render_template("index.html", name=username)
