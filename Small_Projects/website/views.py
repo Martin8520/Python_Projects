b2345f7a9f6b11ad12a710d3bf29cd2342ef5a59
Martin8520 <martin.b8520@gmail.com>
2024-03-23 15:13:12 +0200

    Redirect to JSON.

--- Small_Projects/website/views.py
+++ Small_Projects/website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, jsonify
+from flask import Blueprint, render_template, request, jsonify, redirect ,url_for
 
 views = Blueprint(__name__, "views")
 
@@ -18,3 +18,14 @@ def profile():
 @views.route("/json")
 def get_json():
     return jsonify({"name": "Tim", "age": "35", "address": "125 Homer Street"})
+
+
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+
+@views.route("/go-to-json")
+def go_to_home():
+    return redirect(url_for("views.get_json"))
