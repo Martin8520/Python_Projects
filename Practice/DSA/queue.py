25cc5a157d0ae4b521ccbc633de8c6f939c2ed56
Martin8520 <martin.b8520@gmail.com>
2024-04-06 12:26:02 +0300

    queue example.

---
+++ Practice/DSA/queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+
+queue = Queue()
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+print(queue.dequeue())
