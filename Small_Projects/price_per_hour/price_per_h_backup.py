e38fe54585b4888e06cdd9e773ed67f9305d0aad
Martin8520 <martin.b8520@gmail.com>
2024-04-19 13:49:52 +0300

    Added dates to task and a button that marks when a task was completed

--- Small_Projects/price_per_hour/price_per_h_backup.py
+++ Small_Projects/price_per_hour/price_per_h_backup.py
@@ -5,6 +5,7 @@ from reportlab.lib import colors
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+from datetime import datetime
 
 
 class TaskManager:
@@ -34,27 +35,37 @@ class TaskManager:
         self.entry_price = Entry(master)
         self.entry_price.grid(row=2, column=1, sticky='w')
 
-        self.button_add_task = Button(master, text="Добави Задача", command=self.add_task, width=15, height=1)
+        self.button_add_task = Button(master, text="Добави Задача", command=self.add_task, width=15, height=1,
+                                      anchor='w', justify='left')
         self.button_add_task.grid(row=3, column=0, columnspan=1, sticky='w', padx=(10, 0))
 
-        self.button_edit_task = Button(master, text="Промени Задача", command=self.edit_task, width=15, height=1)
+        self.button_edit_task = Button(master, text="Промени Задача", command=self.edit_task, width=15, height=1,
+                                       anchor='w', justify='left')
         self.button_edit_task.grid(row=4, column=0, columnspan=1, sticky='w', padx=(10, 0))
 
-        self.button_export_pdf = Button(master, text="Запази в PDF", command=self.export_to_pdf, width=15, height=1)
+        self.button_export_pdf = Button(master, text="Запази в PDF", command=self.export_to_pdf, width=15, height=1,
+                                        anchor='w', justify='left')
         self.button_export_pdf.grid(row=5, column=0, columnspan=1, sticky='w', padx=(10, 0))
 
-        self.button_create_csv = Button(master, text="Създай нов файл", command=self.create_csv, width=15, height=1)
+        self.button_create_csv = Button(master, text="Създай нов файл", command=self.create_csv, width=15, height=1,
+                                        anchor='e', justify='right')
         self.button_create_csv.grid(row=3, column=1, columnspan=1, sticky='e', padx=(0, 10))
 
-        self.button_open_csv = Button(master, text="Отвори файл", command=self.open_csv, width=15, height=1)
+        self.button_open_csv = Button(master, text="Отвори файл", command=self.open_csv, width=15, height=1, anchor='e',
+                                      justify='right')
         self.button_open_csv.grid(row=4, column=1, columnspan=1, sticky='e', padx=(0, 10))
 
-        self.button_save_changes = Button(master, text="Запази", command=self.save_changes, width=15, height=1)
+        self.button_save_changes = Button(master, text="Запази", command=self.save_changes, width=15, height=1,
+                                          anchor='e', justify='right')
         self.button_save_changes.grid(row=5, column=1, columnspan=1, sticky='e', padx=(0, 10))
 
         self.button_delete_task = Button(master, text="Изтрий Задача", command=self.delete_task, width=15, height=1,
-                                         bg="#ff6666",)
+                                         bg="#ff6666", anchor='w', justify='left')
         self.button_delete_task.grid(row=11, column=0, columnspan=2, sticky='w', padx=(10, 0))
+
+        self.button_mark_completed = Button(master, text="Маркирай като завършена", command=self.mark_completed,
+                                            width=21, height=1, anchor='e', justify='right')
+        self.button_mark_completed.grid(row=11, column=1, columnspan=1, sticky='e', padx=(0, 10))
 
         self.task_listbox = Listbox(master, width=100, height=10)
         self.task_listbox.grid(row=9, column=0, columnspan=2, sticky='nsew', padx=(10, 0))
@@ -73,7 +84,7 @@ class TaskManager:
         selected_index = self.task_listbox.curselection()
         if selected_index:
             self.selected_index = selected_index[0]
-            task, hours, price = self.tasks[self.selected_index]
+            task, hours, price, start_date, end_date = self.tasks[self.selected_index]
             self.entry_task.delete(0, END)
             self.entry_task.insert(END, task)
             self.entry_hours.delete(0, END)
@@ -100,11 +111,13 @@ class TaskManager:
             try:
                 price = float(price)
             except ValueError:
-                messagebox.showerror("Грешка", "Моля въведете валидно число за цанета.")
+                messagebox.showerror("Грешка", "Моля въведете валидно число за цената.")
                 return
         else:
             price = None
-        self.tasks.append((task, hours, price))
+        start_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        end_date = ""
+        self.tasks.append((task, hours, price, start_date, end_date))
         self.update_task_listbox()
         self.calculate_total()
         self.clear_entries()
@@ -112,10 +125,10 @@ class TaskManager:
     def edit_task(self):
         selected_index = self.task_listbox.curselection()
         if not selected_index:
-            messagebox.showerror("Грешка", "Моля селекетирайте задачата която искате да промените.")
+            messagebox.showerror("Грешка", "Моля селектирайте задачата, която искате да промените.")
             return
         self.selected_index = selected_index[0]
-        task, hours, _ = self.tasks[self.selected_index]
+        task, hours, _, _, _ = self.tasks[self.selected_index]
         edited_task = self.entry_task.get()
         edited_hours = self.entry_hours.get()
         edited_price = self.entry_price.get()
@@ -133,7 +146,7 @@ class TaskManager:
             try:
                 edited_price = float(edited_price)
             except ValueError:
-                messagebox.showerror("Error", "Моля въведете валидно число за цанета.")
+                messagebox.showerror("Грешка", "Моля въведете валидно число за цената.")
                 return
         else:
             edited_price = None
@@ -143,9 +156,21 @@ class TaskManager:
         self.calculate_total()
         self.clear_entries()
 
+    def mark_completed(self):
+        selected_index = self.task_listbox.curselection()
+        if not selected_index:
+            messagebox.showerror("Грешка", "Моля селектирайте задачата, която искате да маркирате като завършена.")
+            return
+        self.selected_index = selected_index[0]
+        task, hours, price, start_date, _ = self.tasks[self.selected_index]
+        end_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.tasks[self.selected_index] = (task, hours, price, start_date, end_date)
+        self.update_task_listbox()
+        self.calculate_total()
+
     def calculate_total(self):
         self.total_price = sum(
-            hours * price for _, hours, price in self.tasks if hours is not None and price is not None)
+            hours * price for _, hours, price, _, _ in self.tasks if hours is not None and price is not None)
         self.label_total_price.config(text=f"Обща цена: {self.total_price} лв.")
 
     def export_to_pdf(self):
@@ -155,11 +180,11 @@ class TaskManager:
 
             pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
 
-            data = [["Задача", "Часове", "Цена на час в лева"]]
-            for task, hours, price in self.tasks:
+            data = [["Задача", "Часове", "Цена на час в лева", "Дата на започване", "Дата на завършване"]]
+            for task, hours, price, start_date, end_date in self.tasks:
                 if hours is not None and price is not None:
-                    data.append([task, str(hours), str(price)])
-            data.append(["Обща цена", "", str(self.total_price) + " лв."])
+                    data.append([task, str(hours), str(price), start_date, end_date])
+            data.append(["Обща цена", "", str(self.total_price) + " лв.", "", ""])
 
             table_style = TableStyle([('BACKGROUND', (0, 0), (-1, 0), colors.grey),
                                       ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
@@ -192,7 +217,9 @@ class TaskManager:
                     task = row[0]
                     hours = float(row[1]) if row[1] else None
                     price = float(row[2]) if row[2] else None
-                    self.tasks.append((task, hours, price))
+                    start_date = row[3]
+                    end_date = row[4]
+                    self.tasks.append((task, hours, price, start_date, end_date))
             self.update_task_listbox()
             self.calculate_total()
 
@@ -202,9 +229,10 @@ class TaskManager:
             return
         with open(self.csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
             csv_writer = csv.writer(csvfile)
-            csv_writer.writerow(['Задача', 'Часове', 'Цена на час в лева'])
-            for task, hours, price in self.tasks:
-                csv_writer.writerow([task, hours if hours is not None else '', price if price is not None else ''])
+            csv_writer.writerow(['Задача', 'Часове', 'Цена на час в лева', 'Дата на започване', 'Дата на завършване'])
+            for task, hours, price, start_date, end_date in self.tasks:
+                csv_writer.writerow([task, hours if hours is not None else '', price if price is not None else '',
+                                     start_date, end_date])
 
     def delete_task(self):
         selected_index = self.task_listbox.curselection()
@@ -217,16 +245,17 @@ class TaskManager:
         if self.csv_filename:
             with open(self.csv_filename, 'w', newline='', encoding='utf-8') as csvfile:
                 csv_writer = csv.writer(csvfile)
-                csv_writer.writerow(['Задача', 'Часове', 'Цена на час в лева'])
-                for task, hours, price in self.tasks:
-                    csv_writer.writerow([task, hours if hours is not None else '', price if price is not None else ''])
+                csv_writer.writerow(['Задача', 'Часове', 'Цена на час в лева', 'Дата на започване', 'Дата на завършване'])
+                for task, hours, price, start_date, end_date in self.tasks:
+                    csv_writer.writerow([task, hours if hours is not None else '', price if price is not None else '',
+                                         start_date, end_date])
 
     def update_task_listbox(self):
         self.task_listbox.delete(0, END)
-        for task, hours, price in self.tasks:
-            self.task_listbox.insert(END, f"Задача: {task}, Часове: {hours if hours is not None else 'N/A'}, Цена на "
-                                          f"час в лева:"
-                                          f" {price if price is not None else 'N/A'} лв.")
+        for task, hours, price, start_date, end_date in self.tasks:
+            self.task_listbox.insert(END, f"Задача: {task}, Часове: {hours if hours is not None else 'N/A'}, "
+                                          f"Цена на час в лева: {price if price is not None else 'N/A'} лв., "
+                                          f"Дата на започване: {start_date}, Дата на завършване: {end_date if end_date else 'Незавършена'}")
 
     def clear_entries(self):
         self.entry_task.delete(0, END)
@@ -236,8 +265,8 @@ class TaskManager:
 
 root = Tk()
 app = TaskManager(root)
-window_width = 650
-window_height = 450
+window_width = 800
+window_height = 550
 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
