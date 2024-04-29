9420e17eea482893b6a48b7010d34ae66eba0643
Martin8520 <martin.b8520@gmail.com>
2024-04-29 09:59:47 +0300

    Practice code.

---
+++ Practice/general_practice/group_anagrams.py
@@ -0,0 +1,21 @@
+def groupAnagrams(strs):
+    anagrams = {}
+
+    for s in strs:
+        sorted_s = ''.join(sorted(s))
+        if sorted_s not in anagrams:
+            anagrams[sorted_s] = [s]
+        else:
+            anagrams[sorted_s].append(s)
+
+    return list(anagrams.values())
+
+
+strs1 = ["eat", "tea", "tan", "ate", "nat", "bat"]
+print(groupAnagrams(strs1))
+
+strs2 = [""]
+print(groupAnagrams(strs2))
+
+strs3 = ["a"]
+print(groupAnagrams(strs3))
