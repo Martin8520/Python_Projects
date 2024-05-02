a5ebd8a4186cbe298fb680a12cbbce3bf7f372de
Martin8520 <martin.b8520@gmail.com>
2024-05-02 13:06:59 +0300

    Practice code.

--- Practice/general_practice/search_2d_matrix.py
+++ Practice/general_practice/search_2d_matrix.py
@@ -21,7 +21,7 @@ def searchMatrix(matrix, target):
 
 matrix1 = [[1, 3, 5, 7], [10, 11, 16, 20], [23, 30, 34, 60]]
 print(searchMatrix(matrix1, 3))
-print(searchMatrix(matrix1, 13))
+print(searchMatrix(matrix1, 10))
 
 matrix2 = [[]]
 print(searchMatrix(matrix2, 1))
