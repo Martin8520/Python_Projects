af92a37e172b31a2c0ff0719c523fef251a440bc
Martin8520 <martin.b8520@gmail.com>
2024-05-04 12:01:04 +0300

    Practice code.

--- Practice/general_practice/scramble_string.py
+++ Practice/general_practice/scramble_string.py
@@ -1,19 +1,34 @@
-def isScramble(s1, s2):
-    if s1 == s2:
-        return True
-    if sorted(s1) != sorted(s2):
-        return False
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        memo = {}
 
-    length = len(s1)
-    for i in range(1, length):
-        if isScramble(s1[:i], s2[:i]) and isScramble(s1[i:], s2[i:]):
-            return True
-        if isScramble(s1[:i], s2[length - i:]) and isScramble(s1[i:], s2[:length - i]):
-            return True
+        def scramble(s1, s2):
+            if (s1, s2) in memo:
+                return memo[(s1, s2)]
 
-    return False
+            if s1 == s2:
+                memo[(s1, s2)] = True
+                return True
+            if sorted(s1) != sorted(s2):
+                memo[(s1, s2)] = False
+                return False
+
+            length = len(s1)
+            for i in range(1, length):
+                if scramble(s1[:i], s2[:i]) and scramble(s1[i:], s2[i:]):
+                    memo[(s1, s2)] = True
+                    return True
+                if scramble(s1[:i], s2[length - i:]) and scramble(s1[i:], s2[:length - i]):
+                    memo[(s1, s2)] = True
+                    return True
+
+            memo[(s1, s2)] = False
+            return False
+
+        return scramble(s1, s2)
 
 
-print(isScramble("great", "rgeat"))
-print(isScramble("abcde", "caebd"))
-print(isScramble("a", "a"))
+solution = Solution()
+print(solution.isScramble("great", "rgeat"))
+print(solution.isScramble("abcde", "caebd"))
+print(solution.isScramble("a", "a"))
