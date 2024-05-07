ba690bdc940865dfd3a6eed56dcabcb60d45cba2
Martin8520 <martin.b8520@gmail.com>
2024-05-07 12:33:30 +0300

    Practice code.

--- Practice/DSA/sorted_squared_array.py
+++ Practice/DSA/sorted_squared_array.py
@@ -1,19 +1,12 @@
-def sorted_squared(arr):
-    n = len(arr)
-    result = [0] * n
-    left, right = 0, n - 1
-    idx = n - 1
-
-    while left <= right:
-        left_squared = arr[left] * arr[left]
-        right_squared = arr[right] * arr[right]
-
-        if left_squared > right_squared:
-            result[idx] = left_squared
-            left += 1
+def sorted_squared(array):
+    n = len(array)
+    i, j = 0, n-1
+    res = [0] * n
+    for k in reversed(range(n)):
+        if array[i] ** 2 > array[j] ** 2:
+            res[k] = array[j] ** 2
+            i += 1
         else:
-            result[idx] = right_squared
-            right -= 1
-        idx -= 1
-
-    return result
+            res[k] = array[j] ** 2
+            j -= 1
+    return res
