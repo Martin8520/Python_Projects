b4e44ca9245584b04ac4e9297acc81d236994dd2
Martin8520 <martin.b8520@gmail.com>
2024-05-09 14:25:24 +0300

    Practice code.

---
+++ Practice/general_practice/same_tree.py
@@ -0,0 +1,34 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def is_same_tree(p, q):
+    if not p and not q:
+        return True
+    if not p or not q:
+        return False
+    if p.val != q.val:
+        return False
+    return is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
+
+
+p = TreeNode(1)
+p.left = TreeNode(2)
+p.right = TreeNode(3)
+
+q = TreeNode(1)
+q.left = TreeNode(2)
+q.right = TreeNode(3)
+
+print(is_same_tree(p, q))
+
+p = TreeNode(1)
+p.left = TreeNode(2)
+
+q = TreeNode(1)
+q.right = TreeNode(2)
+
+print(is_same_tree(p, q))
