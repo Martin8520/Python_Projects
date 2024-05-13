3325c2656aa70bbc6da5c7d08a25c0d6a0f7f6a7
Martin8520 <martin.b8520@gmail.com>
2024-05-13 14:58:51 +0300

    Practice code.

--- Practice/general_practice/valid_palindrome.py
+++ Practice/general_practice/valid_palindrome.py
@@ -1,5 +1,4 @@
 def isPalindrome(s):
-
     def isAlphanumeric(c):
         return c.isalnum()
 
@@ -8,10 +7,6 @@ def isPalindrome(s):
     left, right = 0, len(s) - 1
 
     while left < right:
-        while left < right and not isAlphanumeric(s[left]):
-            left += 1
-        while left < right and not isAlphanumeric(s[right]):
-            right -= 1
         if s[left] != s[right]:
             return False
         left += 1
@@ -21,10 +16,4 @@ def isPalindrome(s):
 
 
 s1 = "A man, a plan, a canal: Panama"
-print(isPalindrome(s1))  # True
-
-s2 = "race a car"
-print(isPalindrome(s2))  # False
-
-s3 = " "
-print(isPalindrome(s3))  # True
+print(isPalindrome(s1))
