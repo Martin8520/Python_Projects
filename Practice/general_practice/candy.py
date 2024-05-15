c0cd1660ccce3513105b648579d2eab3b005a53e
Martin8520 <martin.b8520@gmail.com>
2024-05-15 11:12:54 +0300

    Practice code.

---
+++ Practice/general_practice/candy.py
@@ -0,0 +1,20 @@
+def candy(ratings):
+    n = len(ratings)
+    candies = [1] * n
+
+    for i in range(1, n):
+        if ratings[i] > ratings[i - 1]:
+            candies[i] = candies[i - 1] + 1
+
+    for i in range(n - 2, -1, -1):
+        if ratings[i] > ratings[i + 1]:
+            candies[i] = max(candies[i], candies[i + 1] + 1)
+
+    return sum(candies)
+
+
+ratings1 = [1, 0, 2]
+print(candy(ratings1))  # 5
+
+ratings2 = [1, 2, 2]
+print(candy(ratings2))  # 4
