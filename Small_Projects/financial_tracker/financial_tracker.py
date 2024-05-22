ea49c2620c4b47eb4c414271591599203d8ece91
Martin8520 <martin.b8520@gmail.com>
2024-05-22 11:52:01 +0300

    Created EXE

--- Small_Projects/financial_tracker/financial_tracker.py
+++ Small_Projects/financial_tracker/financial_tracker.py
@@ -6,8 +6,15 @@ from reportlab.lib.pagesizes import letter
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet
+import sys
 
-FILE_NAME = "transactions.csv"
+
+def resource_path(relative_path):
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+
+FILE_NAME = resource_path("transactions.csv")
 
 
 def load_transactions(file_name):
@@ -178,6 +185,7 @@ def export_to_pdf():
     elements.append(balance_paragraph)
 
     doc.build(elements)
+
 
 def main():
     global transactions
