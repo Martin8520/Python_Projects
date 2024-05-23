7ba06ed24ad7d3132ab9946a28ad58798ffd7b71
Martin8520 <martin.b8520@gmail.com>
2024-05-23 11:20:43 +0300

    app compiler

--- Small_Projects/compiler/app_comp.py
+++ Small_Projects/compiler/app_comp.py
@@ -31,6 +31,7 @@ def import_exe(app_name):
     if file_path:
         exe_paths[app_name] = file_path
         save_exe_paths()
+        update_run_button_text(app_name)
         messagebox.showinfo("Import Successful", f"{app_name} imported successfully!")
 
 
@@ -45,12 +46,23 @@ def run_exe(app_name):
         messagebox.showwarning("Warning", f"{app_name} is not imported yet!")
 
 
+def update_run_button_text(app_name):
+    exe_path = exe_paths.get(app_name, "")
+    if exe_path:
+        exe_name = os.path.basename(exe_path)
+        run_buttons[app_name].config(text=f"Run {exe_name}")
+    else:
+        run_buttons[app_name].config(text=f"Run {app_name}")
+
+
 root = tk.Tk()
 root.title("Import and Run EXE Applications")
 
 load_exe_paths()
 
 app_names = ["App1", "App2", "App3"]
+
+run_buttons = {}
 
 for app_name in app_names:
     frame = tk.Frame(root)
@@ -61,5 +73,8 @@ for app_name in app_names:
 
     run_button = tk.Button(frame, text=f"Run {app_name}", command=lambda name=app_name: run_exe(name))
     run_button.pack(side=tk.LEFT, padx=5)
+
+    run_buttons[app_name] = run_button
+    update_run_button_text(app_name)
 
 root.mainloop()
