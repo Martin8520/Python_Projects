8e0c06c3988491922db41826e638873e3f0caf89
Martin8520 <martin.b8520@gmail.com>
2024-05-27 09:30:05 +0300

    Practice code.

--- Practice/general_practice/largest_el_in_array.py
+++ Practice/general_practice/largest_el_in_array.py
@@ -13,7 +13,6 @@ def findKthLargest(nums, k):
     return min_heap[0]
 
 
-# Test cases
 print(findKthLargest([3, 2, 1, 5, 6, 4], 2))  # 5
 print(findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4))  #: 4
 print(findKthLargest([1], 1))  # 1
