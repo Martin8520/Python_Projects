eb76c92af20e273c09c7e19820bd02ada9b3cf99
Martin8520 <martin.b8520@gmail.com>
2024-05-29 12:15:45 +0300

    Practice code.

---
+++ Practice/general_practice/group_anagrams_2.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+
+def groupAnagrams(strs):
+    anagrams = defaultdict(list)
+
+    for word in strs:
+        sorted_word = ''.join(sorted(word))
+        anagrams[sorted_word].append(word)
+
+    return list(anagrams.values())
+
+
+# Test cases
+print(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))  # [["eat","tea","ate"],["tan","nat"],["bat"]]
+print(groupAnagrams([""]))  # [[""]]
+print(groupAnagrams(["a"]))  # [["a"]]
+print(groupAnagrams(["listen", "silent", "enlist"]))  # [["listen","silent","enlist"]]
+print(groupAnagrams(["abc", "bca", "cab", "cba", "bac"]))  # [["abc","bca","cab","cba","bac"]]
