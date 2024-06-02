0310002584827bc79e248dbdc7479478f7e149c5
Martin8520 <martin.b8520@gmail.com>
2024-06-02 12:45:57 +0300

    Practice code

--- Practice/general_practice/longest_increasing_subsequence_dynamic.py
+++ Practice/general_practice/longest_increasing_subsequence_dynamic.py
@@ -1,16 +1,20 @@
+import bisect
+
+
 def length_of_lis(nums):
     if not nums:
         return 0
 
-    n = len(nums)
-    dp = [1] * n
+    lis = []
 
-    for i in range(1, n):
-        for j in range(i):
-            if nums[i] > nums[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
+    for num in nums:
+        pos = bisect.bisect_left(lis, num)
+        if pos == len(lis):
+            lis.append(num)
+        else:
+            lis[pos] = num
 
-    return max(dp)
+    return len(lis)
 
 
 nums1 = [10, 9, 2, 5, 3, 7, 101, 18]
