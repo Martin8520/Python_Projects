6460c3a49d9b9f78a61da87a4c1605994c7a8f96
Martin8520 <martin.b8520@gmail.com>
2024-06-05 14:03:19 +0300

    Practice code

--- Practice/general_practice/tts_reg.py
+++ Practice/general_practice/tts_reg.py
@@ -1,24 +1,13 @@
 import pyttsx3
 
+
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
 
-print("Available Voices:")
-for i, voice in enumerate(voices):
-    print(f"{i+1}. {voice.name}")
-
-selected_index = int(input("Enter the index of the voice you want to use: ")) - 1
-
-if 0 <= selected_index < len(voices):
-    selected_voice = voices[selected_index]
-
-    engine.setProperty('voice', selected_voice.id)
-
-    # Say something
-    engine.say("Hello, I'm speaking using the selected voice.")
-
-    # Wait for speech to finish
-    engine.runAndWait()
-else:
-    print("Invalid voice index. Please select a valid index.")
+print("Available Windows Narrator Voices:")
+for voice in voices:
+    if "narrator" in voice.name.lower():
+        print("Name:", voice.name)
+        print("ID:", voice.id)
+        print()
