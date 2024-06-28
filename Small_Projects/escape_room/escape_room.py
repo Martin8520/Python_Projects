659148be41c24b446e0d37ef092a239c70828e0a
Martin8520 <martin.b8520@gmail.com>
2024-06-28 19:07:55 +0300

    Escape room- basic (WIP)

--- Small_Projects/escape_room/escape_room.py
+++ Small_Projects/escape_room/escape_room.py
@@ -1,7 +1,6 @@
 class Room:
-    def __init__(self, description, items, actions):
+    def __init__(self, description, actions):
         self.description = description
-        self.items = items
         self.actions = actions
 
     def describe(self):
@@ -9,15 +8,12 @@ class Room:
 
     def list_actions(self):
         print("Available actions:")
-        for action in self.actions:
-            print(f"- {action}")
+        for i, action in enumerate(self.actions.keys(), 1):
+            print(f"{i}. {action}")
 
-    def take_action(self, action, inventory):
-        if action in self.actions:
-            return self.actions[action](inventory)
-        else:
-            print("Action not available.")
-            return False
+    def take_action(self, action_num, inventory):
+        action = list(self.actions.keys())[action_num - 1]
+        return self.actions[action](inventory)
 
 
 class Game:
@@ -26,14 +22,14 @@ class Game:
         self.rooms = self.create_rooms()
         self.current_room = 0
         self.actions_taken = 0
+        self.points = 5
 
     def create_rooms(self):
         # Room 1
         room1_description = """
         You are in a dimly lit room. There is a locked door to the north.
-        You see a key on a table, a note on the wall, a bookshelf, a rug, and a chest.
+        You see a key on a table, a note on the wall, and a chest.
         """
-        room1_items = ['key', 'note', 'book', 'hidden compartment', 'lockpick']
 
         def room1_action1(inventory):
             if 'key' not in inventory:
@@ -45,62 +41,29 @@ class Game:
                 return False
 
         def room1_action2(inventory):
-            if 'note' not in inventory:
-                print("You take the note. It has a clue: 'Look under the rug.'")
-                inventory.append('note')
-                return False
-            else:
-                print("You already have the note.")
-                return False
+            print("The note says: 'The key is the way out.'")
+            return False
 
         def room1_action3(inventory):
-            print("You search the bookshelf and find a book titled 'Secrets of the Hidden'.")
-            if 'book' not in inventory:
-                inventory.append('book')
-            return False
-
-        def room1_action4(inventory):
-            print("You lift the rug and find a hidden compartment.")
-            if 'hidden compartment' not in inventory:
-                inventory.append('hidden compartment')
-            return False
-
-        def room1_action5(inventory):
-            if 'hidden compartment' in inventory and 'lockpick' not in inventory:
-                print("You open the hidden compartment and find a lockpick.")
-                inventory.append('lockpick')
-                return False
-            else:
-                print("There is nothing else under the rug.")
-                return False
-
-        def room1_action6(inventory):
-            if 'lockpick' in inventory:
-                print("You unlock the door with the lockpick and escape the room!")
-                return True
-            elif 'key' in inventory:
+            if 'key' in inventory:
                 print("You unlock the door with the key and escape the room!")
                 return True
             else:
-                print("You need a key or a lockpick to unlock the door.")
+                print("You need a key to unlock the door.")
                 return False
 
         room1_actions = {
             'take key': room1_action1,
-            'take note': room1_action2,
-            'search bookshelf': room1_action3,
-            'lift rug': room1_action4,
-            'open compartment': room1_action5,
-            'unlock door': room1_action6
+            'read note': room1_action2,
+            'unlock door': room1_action3
         }
-        room1 = Room(room1_description, room1_items, room1_actions)
+        room1 = Room(room1_description, room1_actions)
 
         # Room 2
         room2_description = """
-        You are in a bright room with a window. There is a locked box, a rope, a painting, a cabinet, and a drawer.
-        You see a knife on the shelf and a map on the floor.
+        You are in a bright room with a window. There is a locked box, a rope, and a painting.
+        You see a knife on the shelf.
         """
-        room2_items = ['knife', 'map', 'key', 'box', 'rope']
 
         def room2_action1(inventory):
             if 'knife' not in inventory:
@@ -112,64 +75,51 @@ class Game:
                 return False
 
         def room2_action2(inventory):
-            if 'map' not in inventory:
-                print("You take the map. It has a clue: 'Look behind the painting.'")
-                inventory.append('map')
-                return False
-            else:
-                print("You already have the map.")
-                return False
-
-        def room2_action3(inventory):
-            print("You look behind the painting and find a hidden key.")
-            if 'key' not in inventory:
-                inventory.append('key')
-            return False
-
-        def room2_action4(inventory):
-            if 'key' in inventory:
-                print("You unlock the box and find a rope.")
-                if 'rope' not in inventory:
-                    inventory.append('rope')
-                return False
-            else:
-                print("You need a key to unlock the box.")
-                return False
-
-        def room2_action5(inventory):
-            print("You open the cabinet and find nothing useful.")
-            return False
-
-        def room2_action6(inventory):
-            if 'knife' in inventory and 'rope' in inventory:
+            if 'knife' in inventory:
                 print("You cut the rope with the knife and use it to escape through the window.")
                 return True
             else:
-                print("You need a knife and a rope to escape.")
+                print("You need a knife to cut the rope.")
                 return False
+
+        def room2_action3(inventory):
+            print("The painting hides a keyhole, but you have no key.")
+            return False
 
         room2_actions = {
             'take knife': room2_action1,
-            'take map': room2_action2,
-            'look behind painting': room2_action3,
-            'unlock box': room2_action4,
-            'open cabinet': room2_action5,
-            'cut rope': room2_action6
+            'cut rope': room2_action2,
+            'inspect painting': room2_action3
         }
-        room2 = Room(room2_description, room2_items, room2_actions)
+        room2 = Room(room2_description, room2_actions)
 
         return [room1, room2]
 
     def play(self):
         print("Welcome to the Escape Room Game!")
-        while self.current_room < len(self.rooms):
+        while self.current_room < len(self.rooms) and self.points > 0:
             self.rooms[self.current_room].describe()
-            while self.actions_taken < 5 or not self.rooms[self.current_room].take_action('unlock door',
-                                                                                          self.inventory):
+            while self.actions_taken < 5 or not self.rooms[self.current_room].take_action(3, self.inventory):
                 self.rooms[self.current_room].list_actions()
-                action = input("Choose an action: ")
-                self.rooms[self.current_room].take_action(action, self.inventory)
-                self.actions_taken += 1
+                try:
+                    action_num = int(input("Choose an action (1, 2, or 3): "))
+                    if action_num not in [1, 2, 3]:
+                        raise ValueError
+                    if self.rooms[self.current_room].take_action(action_num, self.inventory):
+                        break
+                    else:
+                        self.actions_taken += 1
+                        if self.actions_taken >= 5:
+                            print("You have taken the maximum allowed actions for this room.")
+                            break
+                except ValueError:
+                    print("Invalid input. Please enter a number between 1 and 3.")
+                self.points -= 1
+                if self.points > 0:
+                    print(f"Incorrect choice. You lose 1 point. Points remaining: {self.points}")
+                else:
+                    print("You have run out of points. Game over!")
+                    return
             self.actions_taken = 0
             self.current_room += 1
             if self.current_room < len(self.rooms):
