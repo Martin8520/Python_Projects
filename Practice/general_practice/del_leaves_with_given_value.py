7217eebd509b8dd4a79a84ead65d3d347b56cb90
Martin8520 <martin.b8520@gmail.com>
2024-09-13 11:37:20 +0300

    Practice Code

---
+++ Practice/general_practice/del_leaves_with_given_value.py
@@ -0,0 +1,46 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def removeLeafNodes(self, root: TreeNode, target: int) -> TreeNode:
+        if not root:
+            return None
+
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+
+        if not root.left and not root.right and root.val == target:
+            return None
+
+        return root
+
+
+def print_tree(root):
+    if root is None:
+        return []
+    result = []
+    queue = [root]
+    while queue:
+        node = queue.pop(0)
+        if node:
+            result.append(node.val)
+            queue.append(node.left)
+            queue.append(node.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
+
+
+root = TreeNode(1, TreeNode(2), TreeNode(3, TreeNode(2), TreeNode(4)))
+target = 2
+
+sol = Solution()
+new_root = sol.removeLeafNodes(root, target)
+
+print(print_tree(new_root))
