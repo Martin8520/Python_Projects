f05907fc7b51790970c6bb821353e445e00aabba
Martin8520 <martin.b8520@gmail.com>
2024-10-15 13:27:17 +0300

    Practice Code

--- Practice/general_practice/roman_numerals.py
+++ Practice/general_practice/roman_numerals.py
@@ -1,22 +1,48 @@
+def intToRoman(num):
+    roman_dict = [
+        (1000000, '_M'), (900000, '_CM'), (500000, '_D'), (400000, '_CD'),
+        (100000, '_C'), (90000, '_XC'), (50000, '_L'), (40000, '_XL'),
+        (10000, '_X'), (9000, '_IX'), (5000, '_V'), (4000, '_IV'),
+        (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+        (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+        (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+    ]
+
+    roman = ""
+    for value, symbol in roman_dict:
+        while num >= value:
+            roman += symbol
+            num -= value
+    return roman
+
+
 def romanToInt(s):
     roman_dict = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+        '_M': 1000000, '_D': 500000, '_C': 100000, '_L': 50000, '_X': 10000, '_V': 5000,
+        'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1
     }
 
-    s = s.upper()
     total = 0
-    prev_value = 0
-
-    for char in reversed(s):
-        value = roman_dict[char]
-        if value >= prev_value:
-            total += value
+    i = 0
+    while i < len(s):
+        if i < len(s) - 1 and s[i] == '_':
+            value = roman_dict[s[i:i + 2]]
+            i += 2
         else:
-            total -= value
-        prev_value = value
+            value = roman_dict[s[i]]
+            i += 1
+
+        if total > 0 and value > total:
+            total = value - total
+        else:
+            total += value
 
     return total
 
 
-user_input = input("Enter a Roman numeral: ")
-print(romanToInt(user_input))
+user_input = input("Enter a number or a Roman numeral: ")
+
+if user_input.isdigit():
+    print(intToRoman(int(user_input)))
+else:
+    print(romanToInt(user_input))
