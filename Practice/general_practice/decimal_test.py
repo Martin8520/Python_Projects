cfb784ffcb90888db6fddbf305aff4de42a35daf
Martin8520 <martin.b8520@gmail.com>
2024-12-11 17:39:02 +0200

    Practice Code

--- Practice/general_practice/decimal_test.py
+++ Practice/general_practice/decimal_test.py
@@ -7,15 +7,14 @@ min_unit = Decimal('0.21')
 min_percent = Decimal('0.000001')
 
 country_vals = [
-    Decimal('99839'), Decimal('74748'), Decimal('23351'), Decimal('21251'),
-    Decimal('11203'), Decimal('1568'), Decimal('1376'), Decimal('1034'),
-    Decimal('999'), Decimal('996'), Decimal('514'), Decimal('398'),
-    Decimal('203'), Decimal('197'), Decimal('146'), Decimal('81'),
+    Decimal('99839'), Decimal('74848'), Decimal('23353'), Decimal('21251'),
+    Decimal('11203'), Decimal('2394'), Decimal('1568'), Decimal('1376'),
+    Decimal('1344'), Decimal('1032'), Decimal('999'), Decimal('996'),
+    Decimal('514'), Decimal('197'), Decimal('146'), Decimal('81'),
     Decimal('34'), Decimal('12'), Decimal('6'), Decimal('5'),
     Decimal('4'), Decimal('4'), Decimal('2'), Decimal('2'),
     Decimal('2'), Decimal('2'), Decimal('2'), Decimal('2'),
-    Decimal('2'), Decimal('2'), Decimal('0'), Decimal('0.000042'),
-    Decimal('0.420000'), Decimal('0.210000'), Decimal('0.105000'), Decimal('0.000001')
+    Decimal('2'), Decimal('2'), Decimal('0')
 ]
 
 percentages = []
@@ -30,8 +29,8 @@ total_combined = country_total_percentage + server_leftover
 
 print("Calculated percentages for each country:")
 for idx, percentage in enumerate(percentages):
-    print(f"County {idx + 1}: {percentage:.12f}%")
+    print(f"{percentage:.6f}%")
 
-print(f"\nTotal Percentage held by countries: {country_total_percentage:.2f}%")
-print(f"Server Leftover: {server_leftover:.2f}%")
+print(f"\nTotal Percentage held by countries: {country_total_percentage:.6f}%")
+print(f"Server Leftover: {server_leftover:.6f}%")
 print(f"Server total: {total_combined:.2f}%")
